arceuss/Simply-Love-SM5

commit 55a8866c847ad6db870650e372e447aa9b9e7782

fewer unnecessary Actors is better?
Maybe?

So far the only instance I've found where the file MUST return an Actor is ./Graphics/MusicWheelItem Grades/default.lua, so I kept that.

Graphics/Player combo.lua
@@ -1,9 +1,6 @@
 local player = Var "Player"
 
-if SL[ToEnumShortString(player)].ActiveModifiers.HideCombo then
-	return Def.Actor{ InitCommand=function(self) self:visible(false) end }
-end
-
+if SL[ToEnumShortString(player)].ActiveModifiers.HideCombo then return end
 
 local kids
 

BGAnimations/ScreenEvaluation common/PerPlayer/ScatterPlot.lua
@@ -1,6 +1,6 @@
 -- if we're in CourseMode, bail now
 -- the normal LifeMeter graph (Def.GraphDisplay) will be drawn
-if GAMESTATE:IsCourseMode() then return Def.Actor{} end
+if GAMESTATE:IsCourseMode() then return end
 
 -- arguments passed in from Graphs.lua
 local args = ...

BGAnimations/ScreenGameplay underlay/PerPlayer/BackgroundFilter.lua
@@ -2,9 +2,7 @@ local player = ...
 local mods = SL[ ToEnumShortString(player) ].ActiveModifiers
 
 -- if no BackgroundFilter is necessary, it's safe to bail now
-if mods.BackgroundFilter == "Off" then
-	return Def.Actor{ InitCommand=function(self) self:visible(false) end }
-end
+if mods.BackgroundFilter == "Off" then return end
 
 local FilterAlpha = {
 	Dark = 0.5,

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/default.lua
@@ -7,7 +7,7 @@ or SL.Global.Gamestate.Style ~= "single"
 or SL.Global.GameMode == "Casual"
 or (PREFSMAN:GetPreference("Center1Player") and not IsUsingWideScreen())
 then
-	return Def.Actor{ InitCommand=function(self) self:visible(false) end }
+	return
 end
 
 return Def.ActorFrame{

Graphics/MusicWheelItem Grades/default.lua
@@ -1,6 +1,6 @@
 -- if we're in CourseMode or MusicWheel grades are unwanted, return an empty Actor now
 if GAMESTATE:IsCourseMode() or not ThemePrefs.Get("ShowGradesInMusicWheel") then
-	return Def.Actor{}
+	return Def.Actor{ InitCommand=function(self) self:visible(false) end }
 end
 
 

Graphics/Player judgment.lua
@@ -3,9 +3,7 @@ local pn = ToEnumShortString(player)
 local mods = SL[pn].ActiveModifiers
 local kids, JudgmentSet
 
-if mods.JudgmentGraphic == "None" then
-	return Def.Actor{}
-end
+if mods.JudgmentGraphic == "None" then return end
 -- - - - - - - - - - - - - - - - - - - - - -
 
 -- a Judgment might be saved to a profile from a previous GameMode

commit 06d069be199e31dca39e9f93eef594dd85c02c4a

fix error with HideCombo
Sometimes fewer Actors isn't better.  This fixes a bug introduced in 55a8866c847ad6db870650e372e447aa9b9e7782.

Graphics/Player combo.lua
@@ -1,6 +1,8 @@
 local player = Var "Player"
 
-if SL[ToEnumShortString(player)].ActiveModifiers.HideCombo then return end
+if SL[ToEnumShortString(player)].ActiveModifiers.HideCombo then
+	return Def.Actor{ InitCommand=function(self) self:visible(false) end }
+end
 
 local kids
 