arceuss/Simply-Love-SM5

commit 6756226b259f96d54a3826ba544f5b8e02cfa085

Make the QR Pane a conditional pane and merge Oversensitive Pads changes
- The QR code is only relevant for non-course mode. We could add
placeholder text but it looks nicer if just don't have the pane
altogether (although the code isn't nearly as clean =/)
- Merge in the oversensitive pads changes.

BGAnimations/ScreenGameplay overlay/PerColumnJudgmentTracking.lua
@@ -4,15 +4,46 @@ if SL.Global.GameMode == "Casual" then return end
 local player = ...
 local judgments = {}
 for i=1,GAMESTATE:GetCurrentStyle():ColumnsPerPlayer() do
-	judgments[#judgments+1] = { W1=0, W2=0, W3=0, W4=0, W5=0, Miss=0 }
+	judgments[#judgments+1] = { W1=0, W2=0, W3=0, W4=0, W5=0, Miss=0, MissBecauseHeld=0 }
+end
+
+local buttons = {
+	dance = { "Left", "Down", "Up", "Right" },
+	pump = { "DownLeft", "UpLeft", "Center", "UpRight", "DownRight" }
+}
+
+local held = {}
+for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+	held[player] = {
+		dance = { Left=false, Down=false, Up=false, Right=false },
+		pump = { DownLeft=false, UpLeft=false, Center=false, UpRight=false, DownRight=false }
+	}
+end
+
+local current_game = GAMESTATE:GetCurrentGame():GetName()
+
+local InputHandler = function(event)
+	-- if any of these, don't attempt to handle input
+	if not event.PlayerNumber or not event.button then return false end
+
+	if event.type == "InputEventType_FirstPress" then
+		held[event.PlayerNumber][current_game][event.button] = true
+	elseif event.type == "InputEventType_Release" then
+		held[event.PlayerNumber][current_game][event.button] = false
+	end
 end
 
 return Def.Actor{
+	OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback( InputHandler ) end,
     JudgmentMessageCommand=function(self, params)
 		if params.Player == player and params.Notes then
-			for i,col in pairs(params.Notes) do
+			for col,tapnote in pairs(params.Notes) do
 				local tns = ToEnumShortString(params.TapNoteScore)
-				judgments[i][tns] = judgments[i][tns] + 1
+				judgments[col][tns] = judgments[col][tns] + 1
+
+				if tns == "Miss" and held[params.Player][current_game][ buttons[current_game][col] ] then
+					judgments[col].MissBecauseHeld = judgments[col].MissBecauseHeld + 1
+				end
 			end
 		end
     end,

BGAnimations/ScreenEvaluation common/InputHandler.lua
@@ -16,6 +16,13 @@ for player in ivalues(GAMESTATE:GetHumanPlayers()) do
 	for i=1,num_panes do
 		table.insert(panes[pn], af:GetChild(pn.."_AF_Lower"):GetChild("Pane"..i))
 	end
+	-- Increment the num_panes if we intend to add the QR pane.
+	-- NOTE(teejusb): This condition should be should be the same as the one
+	-- defined in default.lua
+	if not GAMESTATE:IsCourseMode() then
+		table.insert(panes[pn], af:GetChild(pn.."_AF_Lower"):GetChild("QRPane"))
+		num_panes = num_panes + 1
+	end
 end
 
 return function(event)

BGAnimations/ScreenEvaluation common/PerPlayer/Pane2/Arrows.lua
@@ -16,7 +16,7 @@ local columns = {
 local box_width = 230
 local column_width = box_width/#columns[game]
 
-local judgments = { "W1", "W2", "W3", "W4", "W5", "Miss" }
+local judgments = { "W1", "W2", "W3", "W4", "W5", "Miss", "MissBecauseHeld" }
 
 -- need to store the number of columns PRIOR to looping
 -- otherwise we enter an infinite loop because the upper bound keeps growing!
@@ -27,7 +27,6 @@ if style == "OnePlayerTwoSides" then
 		table.insert(columns[game], columns[game][i])
 	end
 end
-
 
 local af = Def.ActorFrame{}
 
@@ -48,7 +47,7 @@ for i,column in ipairs( columns[game] ) do
 			Font="_miso",
 			Text=SL[pn].Stages.Stats[SL.Global.Stages.PlayedThisGame + 1].column_judgments[i][judgment],
 			OnCommand=function(self)
-				self:xy(i*column_width-104, _screen.cy-40 + j*24)
+				self:xy(i*column_width-104, _screen.cy-40 + j*22)
 					:zoom(0.9)
 
 				local gmods = SL.Global.ActiveModifiers

BGAnimations/ScreenEvaluation common/PerPlayer/Pane2/JudgmentLabels.lua
@@ -1,7 +1,7 @@
 local player = ...
 local pn = ToEnumShortString(player)
 
-local judgments = { "W1", "W2", "W3", "W4", "W5", "Miss" }
+local judgments = { "W1", "W2", "W3", "W4", "W5", "Miss", "MissBecauseHeld" }
 local TNSNames = {}
 
 local mode = ""
@@ -33,7 +33,7 @@ for index, label in ipairs(TNSNames) do
 		InitCommand=cmd(zoom,0.775; horizalign,right ),
 		BeginCommand=function(self)
 			self:x( (player == PLAYER_1 and -130) or -28 )
-			self:y((index-1)*24 + 8)
+			self:y((index-1)*22 + 8)
 
 			-- if StomperZ, color the JudgmentLabel
 			if mode == "StomperZ" then

BGAnimations/ScreenEvaluation common/PerPlayer/QRPane/default.lua
@@ -7,7 +7,6 @@ local score = FormatPercentScore(PercentDP)
 score = tostring(tonumber((score:gsub("%%", ""))) * 100)
 local failed = stats:GetFailed() and "1" or "0"
 local rate = tostring(SL.Global.ActiveModifiers.MusicRate * 100)
-Trace("Score: " .. score .. " Failed: " .. failed .. " Rate: " .. rate)
 
 local currentSteps = GAMESTATE:GetCurrentSteps(player)
 local difficulty = ""
@@ -28,12 +27,11 @@ local hash = GenerateHash(style, difficulty):sub(1, 12)
 
 local qrcode_size = 168
 local url = ("http://www.groovestats.com/qr.php?h=%s&s=%s&f=%s&r=%s"):format(hash, score, failed, rate)
-Trace(url)
 
 -- ------------------------------------------
 
 local pane = Def.ActorFrame{
-	Name="Pane5",
+	Name="QRPane",
 	InitCommand=function(self)
 		self:visible(false)
 	end
@@ -59,11 +57,6 @@ pane[#pane+1] = Def.Quad{
 pane[#pane+1] = LoadFont("_miso")..{
 	Text="Scan with your phone to upload this score to your GrooveStats account.",
 	InitCommand=function(self) self:zoom(0.8):xy(-140,255):wrapwidthpixels(96/0.8):align(0,0):vertspacing(-4) end
-}
-
-pane[#pane+1] = LoadFont("_miso")..{
-	Text="Coming Soon!",
-	InitCommand=function(self) self:zoom(0.85):xy(-140,344):wrapwidthpixels(145/0.85):align(0,0):vertspacing(-4) end
 }
 
 return pane

BGAnimations/ScreenEvaluation common/default.lua
@@ -1,5 +1,5 @@
 local Players = GAMESTATE:GetHumanPlayers()
-local NumPanes = SL.Global.GameMode=="Casual" and 1 or 5
+local NumPanes = SL.Global.GameMode=="Casual" and 1 or 4
 
 -- Start by loading actors that would be the same whether 1 or 2 players are joined.
 local t = Def.ActorFrame{
@@ -111,6 +111,12 @@ for player in ivalues(Players) do
 	-- add available Panes to the lower ActorFrame via a loop
 	for i=1, NumPanes do
 		lower[#lower+1] = LoadActor("./PerPlayer/Pane"..i, player)
+	end
+	-- Add the QR Pane after the other panes.
+	-- NOTE(teejusb): Since it's a conditional pane, we need to make sure
+	-- we have num_panes set correctly in ./InputHandler
+	if not GAMESTATE:IsCourseMode() then
+	  lower[#lower+1] = LoadActor("./PerPlayer/QRPane", player)
 	end
 
 	-- add lower ActorFrame to the primary ActorFrame

BGAnimations/ScreenEvaluationStage in/default.lua
@@ -1,22 +1,48 @@
--- assume that all human players failed
-local img = "failed text.png"
+local statsP1 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1);
+local statsP2 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2);
 
--- loop through all available human players
-for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-	-- if any of them passed, we want to display the "cleared" graphic
-	if not STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetFailed() then
-		img = "cleared text.png"
+local gradeP1 = statsP1:GetGrade();
+local gradeP2 = statsP2:GetGrade();
+
+local function failed(g)
+	if g == "Grade_Failed" then
+		return true;
+	else
+		return false;
 	end
 end
 
-return Def.ActorFrame {
-	Def.Quad{
-		InitCommand=cmd(FullScreen; diffuse, Color.Black),
-		OnCommand=cmd(sleep,0.2; linear,0.5; diffusealpha,0),
-	},
 
+local img = "cleared text.png"
+
+-- if (only P1) and (P1 failed)
+if (GAMESTATE:IsHumanPlayer(PLAYER_1) and failed(gradeP1) and not GAMESTATE:IsHumanPlayer(PLAYER_2)) then
+	img = "failed text.png"
+	
+-- if (only P2) and (P2 failed)	
+elseif (GAMESTATE:IsHumanPlayer(PLAYER_2) and failed(gradeP2) and not GAMESTATE:IsHumanPlayer(PLAYER_1)) then
+	img = "failed text.png"
+
+-- if (both P1 and P2) and (both P1 and P2 failed)	
+elseif (GAMESTATE:IsHumanPlayer(PLAYER_1) and GAMESTATE:IsHumanPlayer(PLAYER_2) and failed(gradeP1) and failed(gradeP2) ) then
+	img = "failed text.png"
+	
+end
+
+
+local t = Def.ActorFrame {
+	InitCommand=cmd(xy,_screen.cx, _screen.cy);
+	
+	Def.Quad{
+		InitCommand=cmd(zoomto,_screen.w,_screen.h; diffuse,color("0,0,0,1"););
+		OnCommand=cmd(sleep,0.2; linear,0.5;  diffusealpha,0);
+	};
+	
 	LoadActor(img)..{
-		InitCommand=cmd(Center; zoom,0.8; diffusealpha,0),
-		OnCommand=cmd(accelerate,0.4; diffusealpha,1; sleep,0.6; decelerate,0.4; diffusealpha,0)
+		InitCommand=cmd(zoom,0.8; diffusealpha,0;);
+		OnCommand=cmd(accelerate,0.4;diffusealpha,1; sleep,1.3; decelerate,0.4;diffusealpha,0);
 	}
-}
+	
+};
+
+return t;

BGAnimations/ScreenGameOver overlay/PlayerStatsWithoutProfile.lua
@@ -1,8 +1,10 @@
 local player = ...
 
+local stageStats = STATSMAN:GetCurStageStats()
+local currentCombo = stageStats:GetPlayerStageStats(player):GetCurrentCombo()
+
 local totalTime = 0
 local songsPlayedThisGame = 0
-local notesHitThisGame = 0
 
 -- Use pairs here (instead of ipairs) because this player might have late-joined
 -- which will result in nil entries in the the Stats table, which halts ipairs.
@@ -10,21 +12,6 @@ local notesHitThisGame = 0
 for i,stats in pairs( SL[ToEnumShortString(player)].Stages.Stats ) do
 	totalTime = totalTime + (stats and stats.duration or 0)
 	songsPlayedThisGame = songsPlayedThisGame + (stats and 1 or 0)
-
-	if stats and stats.column_judgments then
-		-- increment notesHitThisGame by the total number of tapnotes hit in this particular stepchart by using the per-column data
-		-- don't rely on the engine's non-Miss judgment counts here for two reasons:
-		-- 1. we want jumps/hands to count as more than 1 here
-		-- 2. stepcharts can have non-1 #COMBOS parameters set which would artbitraily inflate notesHitThisGame
-
-		for column, judgments in ipairs(stats.column_judgments) do
-			for judgment, judgment_count in pairs(judgments) do
-				if judgment ~= "Miss" then
-					notesHitThisGame = notesHitThisGame + judgment_count
-				end
-			end
-		end
-	end
 end
 
 local hours = math.floor(totalTime/3600)
@@ -32,9 +19,9 @@ local minutes = math.floor((totalTime-(hours*3600))/60)
 local seconds = round(totalTime%60)
 
 local lines = {
-	ScreenString("SongsPlayedThisGame") .. "\n" .. songsPlayedThisGame,
-	ScreenString("NotesHitThisGame") .. "\n" .. notesHitThisGame,
-	ScreenString("TimeSpentThisGame") .. "\n" .. minutes .. THEME:GetString("ScreenGameOver", "Minutes") .. " " .. seconds .. THEME:GetString("ScreenGameOver", "Seconds")
+	ScreenString("CurrentCombo") .. "\n"..currentCombo,
+	ScreenString("SongsPlayedThisGame") .. "\n"..songsPlayedThisGame,
+	ScreenString("TimeSpentThisGame") .. "\n".. minutes .. THEME:GetString("ScreenGameOver", "Minutes") .. " " .. seconds .. THEME:GetString("ScreenGameOver", "Seconds")
 }
 
 -- assume above that the gameplay session was < 1 hour, but check now

commit 7e65ddc9e1a2ee1f28457d700a0ac03d770037da

clean up PerColumnJudgmentTracking
Commit 6756226b259f96d54a3826ba544f5b8e02cfa085 introduced a bug in PerColumnJudgmentTracking.lua where spamming input on non-joined player during Gameplay would result in Lua errors.

This commit fixes that bug and cleans up the code a little.  All games (the ones choosable from the operator menu anyway) are now handled, the structure of the "held" table makes more sense (there was no need to nest even deeper by current game mode since that won't ever change during ScreenGameplay), and I added a few comments.

This does not attempt to resolve the lingering UI issues ("Miss bc Held") on ScreenEval introduced in 6756226b259f96d54a3826ba544f5b8e02cfa085.

In testing para and kickbox, I better appreciated how many Lua errors are thrown throughout the theme in such game modes.  I don't want to offer full support for every game, but we should try to minimize errors where possible so that players are less likely to get stuck in a broken screen.

---

The floor seemed wonderfully solid. It was comforting to know I had fallen and could fall no farther.

BGAnimations/ScreenGameplay overlay/PerColumnJudgmentTracking.lua
@@ -9,39 +9,52 @@ end
 
 local buttons = {
 	dance = { "Left", "Down", "Up", "Right" },
-	pump = { "DownLeft", "UpLeft", "Center", "UpRight", "DownRight" }
+	pump = { "DownLeft", "UpLeft", "Center", "UpRight", "DownRight" },
+	techno = { "DownLeft", "Left", "UpLeft", "Down", "Up", "UpRight", "Right", "DownRight" },
+	para = { "Left", "UpLeft", "Up", "UpRight", "Right" },
+	beat = { "Key 1", "Key 2", "Key 3", "Key 4", "Key 5", "Key 6", "Key 7", "Scratch up", "Scratch down" },
+	kb7 = { "Key 1", "Key 2", "Key 3", "Key 4", "Key 5", "Key 6", "Key 7" },
+	kickbox = { "Down Left Foot", "Up Left Foot", "Up Left Fist", "Down Left Fist", "Down Right Fist", "Up Right Fist", "Up Right Foot", "Down Right Foot" }
 }
 
+local current_game = GAMESTATE:GetCurrentGame():GetName()
 local held = {}
-for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-	held[player] = {
-		dance = { Left=false, Down=false, Up=false, Right=false },
-		pump = { DownLeft=false, UpLeft=false, Center=false, UpRight=false, DownRight=false }
-	}
+
+-- initialize to handle both players, regardless of whether both are actually joined.
+-- the engine's InputCallback gives you ALL input, so even if only P1 is joined, the
+-- InputCallback will report someone spamming input on P2 as valid events, so we have
+-- to ensure that doesn't cause Lua errors here
+for player in ivalues({PLAYER_1, PLAYER_2}) do
+	held[player] = {}
+
+	-- initialize all buttons available to this game for this player to be "not held"
+	for button in ivalues(buttons[current_game]) do
+		held[player][button] = false
+	end
 end
 
-local current_game = GAMESTATE:GetCurrentGame():GetName()
+
 
 local InputHandler = function(event)
 	-- if any of these, don't attempt to handle input
 	if not event.PlayerNumber or not event.button then return false end
 
 	if event.type == "InputEventType_FirstPress" then
-		held[event.PlayerNumber][current_game][event.button] = true
+		held[event.PlayerNumber][event.button] = true
 	elseif event.type == "InputEventType_Release" then
-		held[event.PlayerNumber][current_game][event.button] = false
+		held[event.PlayerNumber][event.button] = false
 	end
 end
 
 return Def.Actor{
 	OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback( InputHandler ) end,
-    JudgmentMessageCommand=function(self, params)
+	JudgmentMessageCommand=function(self, params)
 		if params.Player == player and params.Notes then
 			for col,tapnote in pairs(params.Notes) do
 				local tns = ToEnumShortString(params.TapNoteScore)
 				judgments[col][tns] = judgments[col][tns] + 1
 
-				if tns == "Miss" and held[params.Player][current_game][ buttons[current_game][col] ] then
+				if tns == "Miss" and held[params.Player][ buttons[current_game][col] ] then
 					judgments[col].MissBecauseHeld = judgments[col].MissBecauseHeld + 1
 				end
 			end