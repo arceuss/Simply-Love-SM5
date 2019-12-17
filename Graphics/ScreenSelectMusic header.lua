arceuss/Simply-Love-SM5

commit 4d3a7e3e2056ade97feec65e0ee128d53cfa1766

fix minor alignment issues with SSM's header text
I spent a few hours tonight trying to figure out what https://github.com/ddrillini/Simply-Love-SM5/issues/17 might be referring to so that there could be one fewer perceived fault with the theme.  This commit contains whatever miscellaneous cleanup was worth keeping after giving up.

I am no closer to divining what "ScreenSummary center header is sized weirdly" might mean, but I am now 104x more confident that bitmap fonts in this theme are a disorganized mess.  Neat.

Graphics/ScreenSelectMusic header.lua
@@ -44,10 +44,11 @@ local t = Def.ActorFrame{
 		InitCommand=function(self)
 			bmt_actor = self
 			if PREFSMAN:GetPreference("EventMode") then
-				self:diffusealpha(0):zoom( WideScale(0.305,0.365) ):xy(_screen.cx, WideScale(10,9))
+				self:zoom( WideScale(0.3,0.36) )
 			else
-				self:diffusealpha(0):zoom( WideScale(0.5,0.6) ):xy(_screen.cx, 15)
+				self:zoom( WideScale(0.5,0.6) )
 			end
+			self:diffusealpha(0):xy(_screen.cx, WideScale(3.15,3.5)/self:GetZoom())
 		end,
 		OnCommand=function(self)
 			if not PREFSMAN:GetPreference("EventMode") then

Graphics/_footer.lua
@@ -1,11 +1,11 @@
-local dark = {0,0,0,0.9}
+-- tables of rgba values
+local dark  = {0,0,0,0.9}
 local light = {0.65,0.65,0.65,1}
 
 return Def.Quad{
 	Name="Footer",
 	InitCommand=function(self)
 		self:draworder(90):zoomto(_screen.w, 32):vertalign(bottom):y(32)
-		
 		if ThemePrefs.Get("RainbowMode") then
 			self:diffuse(dark)
 		else
@@ -15,6 +15,6 @@ return Def.Quad{
 	ScreenChangedMessageCommand=function(self)
 		if SCREENMAN:GetTopScreen():GetName() == "ScreenSelectMusicCasual" then
 			self:diffuse(dark)
-		end	
+		end
 	end
 }

Graphics/_header.lua
@@ -1,4 +1,5 @@
-local dark = {0,0,0,0.9}
+-- tables of rgba values
+local dark  = {0,0,0,0.9}
 local light = {0.65,0.65,0.65,1}
 
 return Def.ActorFrame{

commit 0dda0ee6c09f40041e5013fe752eb69d91de36c0

fix SSM header alignment (again)
This fixes an oversight with alignment introduced in 4d3a7e3e2056ade97feec65e0ee128d53cfa1766.

Graphics/ScreenSelectMusic header.lua
@@ -44,11 +44,13 @@ local t = Def.ActorFrame{
 		InitCommand=function(self)
 			bmt_actor = self
 			if PREFSMAN:GetPreference("EventMode") then
-				self:zoom( WideScale(0.3,0.36) )
+				self:zoom( WideScale(0.3,0.36) ):y( WideScale(3.15,3.5)/self:GetZoom() )
 			else
-				self:zoom( WideScale(0.5,0.6) )
+				self:zoom( WideScale(0.5,0.6) ):y( WideScale(7.5,9)/self:GetZoom() )
 			end
-			self:diffusealpha(0):xy(_screen.cx, WideScale(3.15,3.5)/self:GetZoom())
+
+			self:diffusealpha(0):x(_screen.cx)
+
 		end,
 		OnCommand=function(self)
 			if not PREFSMAN:GetPreference("EventMode") then