arceuss/Simply-Love-SM5

commit 974609ff821b3de7f2ce543546cab6faa953786b

right-align MusicWheelItems (...kind of)
At certain resolutions and aspect ratios, a single pixel gap would appear between the right side of the MusicWheel and the right edge of the screen.  I've been aware of this for a while and assumed it was due to rounding errors because of how badly I'd originally written all the code for ScreenSelectMusic back in 2013.

This only happened at certain resolutions and, to be honest, was only really apparent when RainbowMode was enabled, so I gave it low priority on my list of things to fix.

I set out to fix it today, thinking "easy, I'll just right-align the MusicWheelItems and set their x value to SCREEN_WIDTH."

I'm left with messy, ad-hoc code I am no less embarrassed by that (sigh) does accomplish the goal of eliminating that 1 pixel gap that sometimes appeared.  Spoilers: it accomplishes this by extending the overall width of the MusicWheelItems giving each a bit of overflow that draws beyond the right edge of the screen.

Eyeballing the before and after in 4:3, 16:9, and 16:10, I can say with some assurance that, after several hours of fiddling with numbers and alignment and zoom values, users should not see any difference in the way ScreenSelectMusic looks beyond the 1px gap no longer happening.

It still passes the "How To Cook Delicious Rice and the Effects of Eating Rice" test, so that's something.

metrics.ini
@@ -1403,7 +1403,7 @@ MusicWheelItemSortOffCommand=decelerate,0.2;zoomy,0
 
 # Highlight Commands
 HighlightOnCommand=%function(self) \
-	if ThemePrefs.Get("RainbowMode") then \
+	if DarkUI() then \
 		self:diffuseshift():effectcolor1(0.8,0.8,0.8,0.35):effectcolor2(0.1,0.1,0.1,0.35):effectclock("beatnooffset"):effectperiod(2) \
 	else \
 		self:diffuseshift():effectcolor1(0.8,0.8,0.8,0.3):effectcolor2(0.8,0.8,0.8,0.05):effectclock("beatnooffset"):effectperiod(2) \
@@ -1421,20 +1421,26 @@ CourseOnCommand=maxwidth,WideScale(240,310)
 SectionExpandedOnCommand=maxwidth,WideScale(240,310)
 SectionCollapsedOnCommand=maxwidth,WideScale(240,310)
 
-SectionCountX=_screen.w/4 + WideScale(1,0)
-SectionCountOnCommand=halign,1;zoom,0.75
+SectionExpandedX=WideScale(150, 204)
+SectionCollapsedX=WideScale(150, 204)
+RouletteX=WideScale(150, 204)
+RandomX=WideScale(150, 204)
+PortalX=WideScale(150, 204)
+
+SectionCountX=_screen.w/2 - WideScale(9, 10)
+SectionCountOnCommand=horizalign,right;zoom,0.75
 SectionCountSetCommand=%function(self) \
 	if ThemePrefs.Get("nice") > 0 and self:GetText()=="69" then \
-		self:settext("nice 👌") \
+		self:settext("nice") \
 	end \
 end
 RandomOnCommand=diffuseshift;effectcolor1,color("1,1,1,1");effectcolor2,color("#88EEFF");effectperiod,10;shadowlength,2
 
 WheelNotifyIconOnCommand=visible,false
 
-GradeP1X=WideScale(-112,-154)
+GradeP1X=WideScale(38,50)
 GradeP1Y=-0
-GradeP2X=WideScale(-95,-124)
+GradeP2X=WideScale(54,80)
 GradeP2Y=0
 GradesShowMachine=false
 
@@ -1442,8 +1448,8 @@ GradesShowMachine=false
 GetReturnsNumbers=true
 
 [TextBanner]
-TitleOnCommand=halign,0;strokecolor,color("0,0,0,0.25");x,WideScale(-85, -100);maxwidth,WideScale(300,378);zoom,0.85
-SubtitleOnCommand=halign,0;strokecolor,color("0,0,0,0.25");x,WideScale(-85, -100);maxwidth,WideScale(300,378);zoom,0.7
+TitleOnCommand=halign,0;strokecolor,color("0,0,0,0.25");x,WideScale(65, 104);maxwidth,WideScale(270,350);zoom,0.85
+SubtitleOnCommand=halign,0;strokecolor,color("0,0,0,0.25");x,WideScale(65, 104);maxwidth,WideScale(270,350);zoom,0.7
 ArtistOnCommand=visible,false
 AfterSetCommand=%TextBannerAfterSet
 
@@ -1501,7 +1507,7 @@ HeaderOnCommand=draworder,101
 FooterOnCommand=draworder,101
 
 # wheel
-MusicWheelX=_screen.cx+_screen.w/4-10
+MusicWheelX=_screen.cx
 MusicWheelY=_screen.cy
 MusicWheelOffCommand=linear,0.2;diffusealpha,0;
 

BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
@@ -1,5 +1,4 @@
--- Pane3 displays a list of HighScores for the stepchart that was played
--- using the machine profile.
+-- Pane3 displays a list of HighScores for the stepchart that was played.
 
 local player = ...
 

Graphics/MusicWheel highlight.lua
@@ -1,1 +1,8 @@
-return Def.Quad{ InitCommand=function(self) self:zoomto(_screen.w/2.1675,_screen.h/15-1):x(26) end }
+local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
+-- subtract 2 from the total number of MusicWheelItems
+-- one MusicWheelItem will be offsceen above, one will be offscreen below
+local num_visible_items = num_items - 2
+
+local item_width = _screen.w/WideScale(2.125, 2.125)
+
+return Def.Quad{ InitCommand=function(self) self:horizalign(left):x(WideScale(28,33)):zoomto(item_width,_screen.h/num_visible_items-1) end }

Graphics/MusicWheelItem Course NormalPart.lua
@@ -1,20 +1,14 @@
-return Def.ActorFrame{
-	InitCommand=function(self) self:x(26) end,
+local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
+-- subtract 2 from the total number of MusicWheelItems
+-- one MusicWheelItem will be offsceen above, one will be offscreen below
+local num_visible_items = num_items - 2
 
-	Def.Quad{
-		InitCommand=function(self)
-			self:diffuse(0, 10/255, 17/255, 0.5) -- #000a11
-			:zoomto(_screen.w/2.1675, _screen.h/15)
-		end
-	},
-	Def.Quad{
-		InitCommand=function(self)
-			if ThemePrefs.Get("RainbowMode") then
-				self:diffuse(1,1,1,0.5)
-			else
-				self:diffuse(10/255, 20/255, 27/255, 1) -- #0a141b
-			end
-			self:zoomto(_screen.w/2.1675, _screen.h/15 - 1)
-		end
-	}
+local item_width = _screen.w/WideScale(2.125, 2.125)
+
+return Def.ActorFrame{
+	-- the MusicWheel is centered via metrics under [ScreenSelectMusic]; offset by a slight amount to the right here
+	InitCommand=function(self) self:x(WideScale(28,33)) end,
+
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(0, 10/255, 17/255, 0.5):zoomto(item_width, _screen.h/num_visible_items) end },
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(DarkUI() and {1,1,1,0.5} or {10/255, 20/255, 27/255, 1}):zoomto(item_width, (_screen.h/num_visible_items)-1) end }
 }

Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
@@ -1,6 +1,14 @@
-return Def.ActorFrame{
-	InitCommand=function(self) self:x(26) end,
+local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
+-- subtract 2 from the total number of MusicWheelItems
+-- one MusicWheelItem will be offsceen above, one will be offscreen below
+local num_visible_items = num_items - 2
 
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(_screen.w/2.1675, _screen.h/15) end },
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#283239")):zoomto(_screen.w/2.1675, _screen.h/15 - 1) end }
+local item_width = _screen.w/WideScale(2.125, 2.125)
+
+return Def.ActorFrame{
+	-- the MusicWheel is centered via metrics under [ScreenSelectMusic]; offset by a slight amount to the right here
+	InitCommand=function(self) self:x(WideScale(28,33)) end,
+
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(color("#000000")):zoomto(item_width, _screen.h/num_visible_items) end },
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(color("#283239")):zoomto(item_width, _screen.h/num_visible_items - 1) end }
 }

Graphics/MusicWheelItem SectionExpanded NormalPart.lua
@@ -1,6 +1,14 @@
-return Def.ActorFrame{
-	InitCommand=function(self) self:x(26) end,
+local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
+-- subtract 2 from the total number of MusicWheelItems
+-- one MusicWheelItem will be offsceen above, one will be offscreen below
+local num_visible_items = num_items - 2
 
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(_screen.w/2.1675, _screen.h/15) end },
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#4c565d")):zoomto(_screen.w/2.1675, _screen.h/15 - 1) end }
+local item_width = _screen.w/WideScale(2.125, 2.125)
+
+return Def.ActorFrame{
+	-- the MusicWheel is centered via metrics under [ScreenSelectMusic]; offset by a slight amount to the right here
+	InitCommand=function(self) self:x(WideScale(28,33)) end,
+
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(color("#000000")):zoomto(item_width, _screen.h/num_visible_items) end },
+	Def.Quad{ InitCommand=function(self) self:horizalign(left):diffuse(color("#4c565d")):zoomto(item_width, _screen.h/num_visible_items - 1) end }
 }

Graphics/MusicWheelItem Song NormalPart.lua
@@ -9,8 +9,10 @@ local stepstype = GAMESTATE:GetCurrentStyle():GetStepsType()
 af[#af+1] = Def.Sprite{
 	Texture=THEME:GetPathG("", "Has Edit (doubleres).png"),
 	InitCommand=function(self)
-		self:visible(false):x(WideScale(130,182)):zoom(0.375)
-		if ThemePrefs.Get("RainbowMode") then self:diffuse(0,0,0,1) end
+		self:horizalign(left):visible(false):zoom(0.375)
+		self:x( _screen.w/(WideScale(2.15, 2.14)) - self:GetWidth()*self:GetZoom() - 8 )
+
+		if DarkUI() then self:diffuse(0,0,0,1) end
 	end,
 	SetCommand=function(self, params)
 		self:visible(params.Song and params.Song:HasEdits(stepstype) or false)

commit 28aba23b219f156dba6593156b72dc38db15fa98

fix MusicWheel text positioning in CourseMode
This fixes the text-positioning of the CourseMode MusicWheel.  I broke it in
974609ff821b3de7f2ce543546cab6faa953786b.

This also provides a different (more appropriate?) fix for ScreenEditMenu's text
positioning.  It was originally broken in the same commit, and I first fixed it in
8843f16640ea6dd88d9a9eecd250b05b14aa15ea.

BGAnimations/ScreenEditMenu underlay.lua
@@ -17,11 +17,6 @@ end
 local t = Def.ActorFrame{
 	InitCommand=function(self) self:diffusealpha(0) end,
 	OnCommand=function(self)
-		local topscreen = SCREENMAN:GetTopScreen()
-		if topscreen then
-			topscreen:GetChild("EditMenu"):GetChild("SongTextBanner"):addx(WideScale(-150,-204))
-		end
-
 		self:linear(0.15):diffusealpha(1):queuecommand("ApplyColor")
 	end,
 	EditMenuChangeMessageCommand=function(self) self:playcommand("ApplyColor") end,

metrics.ini
@@ -1424,8 +1424,7 @@ end
 SectionCollapsedSetCommand=%function(self) DiffuseEmojis(self) end
 SectionExpandedSetCommand=%function(self) DiffuseEmojis(self) end
 
-
-CourseOnCommand=maxwidth,WideScale(240,310)
+CourseOnCommand=halign,0;x,WideScale(65, 104);maxwidth,WideScale(270,350);zoom,0.85
 
 # ensure that very long titles don't go off-screen
 SectionExpandedOnCommand=maxwidth,WideScale(240,310)
@@ -1542,6 +1541,7 @@ HelpText=ScreenString("HelpTextSelectCourse")
 
 [CourseWheel]
 Fallback="MusicWheel"
+
 
 
 [ScreenSelectCourseNonstop]
@@ -1872,7 +1872,7 @@ SongBannerHeight=90
 SongBannerWidth=36
 SongBannerOnCommand=scaletoclipped,90,36
 
-SongTextBannerX=_screen.cx - WideScale(45,30)
+SongTextBannerX=_screen.cx - WideScale(195,233)
 SongTextBannerY=_screen.cy-111
 
 StepsDisplayX=_screen.cx+170