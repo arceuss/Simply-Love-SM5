f6c899aa3c5516666e69c91099b6d9b4aa34219b
Martin Natano <natano@natano.net>
2021-05-02 22:42:33 +0200

    Add another visual style

--- BGAnimations/ScreenTitleMenu underlay/Logo.lua
+++ BGAnimations/ScreenTitleMenu underlay/Logo.lua
@@ -6,33 +6,44 @@ if game ~= "dance" and game ~= "pump" then
 	game = "techno"
 end
 
-
--- -----------------------------------------------------------------------
-local style = ThemePrefs.Get("VisualStyle")
-local image = "TitleMenu"
-
--- see: watch?v=wxBO6KX9qTA etc.
-if FILEMAN:DoesFileExist("/Themes/"..THEME:GetCurThemeName().."/Graphics/_VisualStyles/"..ThemePrefs.Get("VisualStyle").."/TitleMenuAlt (doubleres).png") then
-	if math.random(1,100) <= 10 then image="TitleMenuAlt" end
-end
-
 -- -----------------------------------------------------------------------
 local af = Def.ActorFrame{}
 
 -- SIMPLY [something]
-af[#af+1] = LoadActor(THEME:GetPathG("", "_VisualStyles/"..style.."/"..image.." (doubleres).png"))..{
+af[#af+1] = Def.Sprite{
 	Name="Simply Text",
 	InitCommand=function(self)
-		self:zoom(0.7):vertalign(top)
-		self:y(-102):shadowlength(0.75)
+		self:playcommand("LoadImage")
 	end,
-	OffCommand=function(self) self:linear(0.5):shadowlength(0) end
+	OffCommand=function(self) self:linear(0.5):shadowlength(0) end,
+	VisualStyleSelectedMessageCommand=function(self)
+		self:playcommand("LoadImage")
+	end,
+	LoadImageCommand=function(self)
+		if ThemePrefs.Get("VisualStyle") == "SRPG5" then
+			self:Load(THEME:GetPathG("", "_VisualStyles/SRPG5/"..SL.SRPG5.GetLogo()))
+			self:zoom(0.7):vertalign(top)
+			self:y(-110):shadowlength(0)
+		else
+			local style = ThemePrefs.Get("VisualStyle")
+			local image = THEME:GetPathG("", "_VisualStyles/"..style.."/TitleMenu (doubleres).png")
+			local imageAlt = "/Themes/"..THEME:GetCurThemeName().."/Graphics/_VisualStyles/"..style.."/TitleMenuAlt (doubleres).png"
+			if FILEMAN:DoesFileExist(imageAlt) and math.random(1,100) <= 10 then
+				self:Load(imageAlt)
+			else
+				self:Load(image)
+			end
+			self:zoom(0.7):vertalign(top)
+			self:y(-102):shadowlength(0.75)
+		end
+	end,
 }
 
 -- decorative arrows
 af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
 	InitCommand=function(self)
 		self:y(-16)
+		self:visible(ThemePrefs.Get("VisualStyle") ~= "SRPG5")
 
 		-- get a reference to the SIMPLY [something] graphic
 		-- it's rasterized text in the Wendy font like "SIMPLY LOVE" or "SIMPLY THONK" or etc.
@@ -46,7 +57,10 @@ af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
 		-- so, get the horizontal zoom factor of these decorative arrows
 		-- and apply it to the y-axis as well to maintain proportions
 		self:zoomy( self:GetZoomX() )
-	end
+	end,
+	VisualStyleSelectedMessageCommand=function(self)
+		self:visible(ThemePrefs.Get("VisualStyle") ~= "SRPG5")
+	end,
 }
 
 return af
